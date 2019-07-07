097ef196bccc2a7686be34b43249edd1d48d6f9c
luan <almeida.luan099@gmail.com>
2019-07-06 22:59:35 -0300

    Configuração do Spring boot admin feita,porem deveria ser criada em um outro projeto, security das rotas barrando ficar no mesmo projeto

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -1,13 +1,19 @@
 package br.com.alura.forum;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
 @EnableCaching
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
 public class ForumApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -43,6 +43,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.authorizeRequests().antMatchers(HttpMethod.GET,"/topicos").permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.GET,"/**").permitAll();
         http.authorizeRequests().antMatchers(HttpMethod.POST,"/auth").permitAll();
         http.authorizeRequests().antMatchers(HttpMethod.GET,"/topicos/*").permitAll();
         http.authorizeRequests().antMatchers(HttpMethod.GET,"/actuator/**").permitAll()
