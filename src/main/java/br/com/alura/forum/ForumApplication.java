e0f7a8308bd93d19987fd110e43205e95ab59f06
luan <almeida.luan099@gmail.com>
2019-07-20 18:03:02 -0300

    Separando Spring Admin do projeto e repassando para um outro de monitoramento

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -1,6 +1,5 @@
 package br.com.alura.forum;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
